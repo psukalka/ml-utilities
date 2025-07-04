8efbe258d2bd031225ea1730c1a365d89b3f7383
Pavan Sukalkar <pavan.s@kukufm.com>
2025-07-04 09:46:15 +0530

    More fixes

--- scripts/regressions.py
+++ scripts/regressions.py
@@ -14,17 +14,17 @@ def linear_regression():
     print(len(X), len(y), len(X_train), len(X_test), len(y_train), len(y_test))
     print(f"--- Linear Regression training ---")
     trained_regressor = rh.train_with_linear_regressor(X_train, y_train)
-    y_train_pred = rh.predict_with_linear_regressor(trained_regressor, X_train)
-    visualize(X_train, y_train, y_train_pred)
+    visualize(X_train, y_train, trained_regressor)
+    visualize(X_test, y_test, trained_regressor, type='test')
 
 
-def visualize(X_train, y_train, y_train_pred):
+def visualize(X_train, y_train, trained_regressor, type='training'):
     plt.scatter(X_train, y_train, color='red')
-    plt.plot(X_train, y_train_pred, color='blue')
-    plt.title('Salary vs Experience (Training Set)')
+    plt.plot(X_train, rh.predict_with_linear_regressor(trained_regressor, X_train), color='blue')
+    plt.title(f'Salary vs Experience ({type} Set)')
     plt.xlabel('Years of Experience')
     plt.ylabel('Salary')
-    plt.savefig('linear_regression_training_data.png')
+    plt.savefig(f'linear_regression_{type}_data.png')
     plt.close()
 
 if __name__ == "__main__":
